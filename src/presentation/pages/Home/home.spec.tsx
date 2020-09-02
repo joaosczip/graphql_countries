09e3242e47b8553793d2f9f4685cc4529c42ef10
João Guilherme <joao.sczip@hotmail.com>
2020-09-02 00:54:34 -0300

    feat: move sut creation to a factory method

--- src/presentation/pages/Home/home.spec.tsx
+++ src/presentation/pages/Home/home.spec.tsx
@@ -3,10 +3,21 @@ import { render } from "@testing-library/react";
 import Home from ".";
 import { LoadCountriesSpy } from "@/presentation/test";
 
+type Sut = {
+  loadCountriesSpy: LoadCountriesSpy;
+};
+
+const sutFactory = (): Sut => {
+  const loadCountriesSpy = new LoadCountriesSpy();
+  render(<Home loadCountries={loadCountriesSpy} />);
+  return {
+    loadCountriesSpy,
+  };
+};
+
 describe("Home", () => {
   it("should calls LoadCountries on load", () => {
-    const loadCountriesSpy = new LoadCountriesSpy();
-    render(<Home loadCountries={loadCountriesSpy} />);
+    const { loadCountriesSpy } = sutFactory();
     expect(loadCountriesSpy.callsCount).toBe(1);
     expect(loadCountriesSpy.params).toEqual({
       offset: 0,
