d18f08d396c30d62da7565802270b70c6d38a1e9
João Guilherme <joao.sczip@hotmail.com>
2020-09-01 22:42:45 -0300

    feat: ensure GraphqlCountriesRepository calls ApolloClient.query with correct values

--- src/infra/repositories/graphql-countries-repository.ts
+++ src/infra/repositories/graphql-countries-repository.ts
@@ -1,4 +1,5 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import fetch from "cross-fetch";
+import { ApolloClient, InMemoryCache, gql, HttpLink } from "@apollo/client";
 import { LoadCountriesRepository } from "@/data/protocols";
 
 export class GraphqlCountriesRepository implements LoadCountriesRepository {
@@ -6,12 +7,28 @@ export class GraphqlCountriesRepository implements LoadCountriesRepository {
 
   constructor() {
     this.client = new ApolloClient({
-      uri: process.env.API_URI,
+      link: new HttpLink({
+        uri: process.env.API_URI,
+        fetch,
+      }),
       cache: new InMemoryCache(),
     });
   }
 
   async loadAll(): Promise<LoadCountriesRepository.Result> {
+    const query = gql`
+      {
+        Country {
+          _id
+          name
+          capital
+          flag {
+            svgFile
+          }
+        }
+      }
+    `;
+    await this.client.query({ query });
     return null;
   }
 }
