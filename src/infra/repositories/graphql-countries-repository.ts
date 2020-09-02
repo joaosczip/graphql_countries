cfeccb34f96cc7798750416b5f11b5b3000d1817
João Guilherme <joao.sczip@hotmail.com>
2020-09-01 21:49:30 -0300

    chore: add ApolloClient config

--- src/infra/repositories/graphql-countries-repository.ts
+++ src/infra/repositories/graphql-countries-repository.ts
@@ -1,6 +1,16 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { LoadCountriesRepository } from "@/data/protocols";
 
 export class GraphqlCountriesRepository implements LoadCountriesRepository {
+  private client: ApolloClient<any>;
+
+  constructor() {
+    this.client = new ApolloClient({
+      uri: process.env.API_URI,
+      cache: new InMemoryCache(),
+    });
+  }
+
   async loadAll(): Promise<LoadCountriesRepository.Result> {
     return null;
   }
