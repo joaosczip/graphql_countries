01822cb8ef441d6d907932c99a7418c3e3daf394
João Guilherme <joao.sczip@hotmail.com>
2020-09-02 13:39:35 -0300

    chore: update jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{ts,tsx}",
+    "!<rootDir>/src/index.tsx",
+    "!<rootDir>/src/**/*.d.ts",
     "!<rootDir>/src/main/**/*",
     "!<rootDir>/src/presentation/@types/**/*",
     "!<rootDir>/src/presentation/styles/*.ts",
