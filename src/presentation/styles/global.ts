1fe679cbe862a41f6d95a7e4da508808bc24ecf0
João Guilherme <joao.sczip@hotmail.com>
2020-09-01 23:26:37 -0300

    fix: yarn @types problem

---
+++ src/presentation/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f1f1f1;
+    height: 100%;
+  }
+
+  h1, h2, h3, h4, h5, span, p {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
