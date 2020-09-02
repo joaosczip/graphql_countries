3b86af20516ae063353edea6cca219ca8fa323ec
João Guilherme <joao.sczip@hotmail.com>
2020-09-02 17:38:41 -0300

    test: ensure GraphqlCountriesRepository calls apollo.query with correct query

--- src/infra/repositories/graphql-countries-repository.spec.ts
+++ src/infra/repositories/graphql-countries-repository.spec.ts
@@ -1,7 +1,7 @@
 import faker from "faker";
 import { ApolloClient, gql, ApolloQueryResult } from "@apollo/client/";
 import { GraphqlCountriesRepository } from "./graphql-countries-repository";
-import { mockBasicCountry } from "@/domain/test";
+import { mockBasicCountry, mockCountry } from "@/domain/test";
 import { LoadCountriesRepository } from "@/data/protocols";
 
 jest.mock("@apollo/client");
@@ -33,78 +33,131 @@ const mockQueryResult = (
   },
 });
 
+const mockLoadQueryResult = () => {
+  const { id, topLevelDomain, flag, ...country } = mockCountry();
+  return {
+    data: {
+      Country: [
+        {
+          ...country,
+          flag: {
+            svgFile: faker.internet.url(),
+          },
+          topLevelDomains: [
+            {
+              name: faker.random.word(),
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
+
 const makeFakeParams = (): LoadCountriesRepository.Params => ({
   offset: faker.random.number(),
   limit: faker.random.number(),
 });
 
 describe("GraphqlCountriesRepository", () => {
-  it("should calls apollo.query with correct data", async () => {
-    const sut = sutFactory();
-    const query = gql`
-      {
-        Country(first: 12, offset: 0) {
-          _id
-          name
-          capital
-          flag {
-            svgFile
+  describe("loadAll", () => {
+    it("should calls apollo.query with correct data", async () => {
+      const sut = sutFactory();
+      const query = gql`
+        {
+          Country(first: 12, offset: 0) {
+            _id
+            name
+            capital
+            flag {
+              svgFile
+            }
           }
         }
-      }
-    `;
-    const querySpy = jest.spyOn(ApolloClient.prototype, "query");
-    querySpy.mockResolvedValueOnce(mockQueryResult() as ApolloQueryResult<any>);
-    await sut.loadAll({
-      offset: 0,
-      limit: 12,
+      `;
+      const querySpy = jest.spyOn(ApolloClient.prototype, "query");
+      querySpy.mockResolvedValueOnce(
+        mockQueryResult() as ApolloQueryResult<any>
+      );
+      await sut.loadAll({
+        offset: 0,
+        limit: 12,
+      });
+      expect(querySpy).toHaveBeenCalledWith({ query });
     });
-    expect(querySpy).toHaveBeenCalledWith({ query });
+    it("should returns null if apollo.query dont return Country", async () => {
+      const sut = sutFactory();
+      jest.spyOn(ApolloClient.prototype, "query").mockResolvedValueOnce({
+        data: {},
+      } as ApolloQueryResult<any>);
+      const result = await sut.loadAll(makeFakeParams());
+      expect(result).toBeNull();
+    });
+    it("should returns null if apollo.query returns empty Country", async () => {
+      const sut = sutFactory();
+      jest.spyOn(ApolloClient.prototype, "query").mockResolvedValueOnce({
+        data: {
+          Country: [],
+        },
+      } as ApolloQueryResult<any>);
+      const result = await sut.loadAll(makeFakeParams());
+      expect(result).toBeNull();
+    });
+    it("should throws if apollo.query throws", () => {
+      const sut = sutFactory();
+      jest
+        .spyOn(ApolloClient.prototype, "query")
+        .mockRejectedValueOnce(new Error());
+      const result = sut.loadAll(makeFakeParams());
+      expect(result).rejects.toThrow(new Error());
+    });
+    it("should returns the countries on success", async () => {
+      const sut = sutFactory();
+
+      const firstCountry = mockBasicCountry();
+      const secondCountry = mockBasicCountry();
+
+      const basicCountries = mockQueryResult(firstCountry, secondCountry);
+
+      jest
+        .spyOn(ApolloClient.prototype, "query")
+        .mockResolvedValueOnce(basicCountries as ApolloQueryResult<any>);
+
+      const result = await sut.loadAll(makeFakeParams());
+
+      const expected = [
+        { ...firstCountry, flag: basicCountries.data.Country[0].flag.svgFile },
+        { ...secondCountry, flag: basicCountries.data.Country[1].flag.svgFile },
+      ];
+      expect(result).toEqual(expected);
+    });
   });
-  it("should returns null if apollo.query dont return Country", async () => {
-    const sut = sutFactory();
-    jest.spyOn(ApolloClient.prototype, "query").mockResolvedValueOnce({
-      data: {},
-    } as ApolloQueryResult<any>);
-    const result = await sut.loadAll(makeFakeParams());
-    expect(result).toBeNull();
-  });
-  it("should returns null if apollo.query returns empty Country", async () => {
-    const sut = sutFactory();
-    jest.spyOn(ApolloClient.prototype, "query").mockResolvedValueOnce({
-      data: {
-        Country: [],
-      },
-    } as ApolloQueryResult<any>);
-    const result = await sut.loadAll(makeFakeParams());
-    expect(result).toBeNull();
-  });
-  it("should throws if apollo.query throws", () => {
-    const sut = sutFactory();
-    jest
-      .spyOn(ApolloClient.prototype, "query")
-      .mockRejectedValueOnce(new Error());
-    const result = sut.loadAll(makeFakeParams());
-    expect(result).rejects.toThrow(new Error());
-  });
-  it("should returns the countries on success", async () => {
-    const sut = sutFactory();
-
-    const firstCountry = mockBasicCountry();
-    const secondCountry = mockBasicCountry();
-
-    const basicCountries = mockQueryResult(firstCountry, secondCountry);
-
-    jest
-      .spyOn(ApolloClient.prototype, "query")
-      .mockResolvedValueOnce(basicCountries as ApolloQueryResult<any>);
-
-    const result = await sut.loadAll(makeFakeParams());
-
-    const expected = [
-      { ...firstCountry, flag: basicCountries.data.Country[0].flag.svgFile },
-      { ...secondCountry, flag: basicCountries.data.Country[1].flag.svgFile },
-    ];
-    expect(result).toEqual(expected);
+  describe("load", () => {
+    it("should calls apollo.query with correct data", async () => {
+      const sut = sutFactory();
+      const countryId = faker.random.number();
+      const query = gql`
+        {
+          Country(_id: ${countryId}) {
+            name
+            area
+            population
+            capital
+            flag {
+              svgFile
+            }
+            topLevelDomains {
+              name
+            }
+          }
+        }
+      `;
+      const querySpy = jest.spyOn(ApolloClient.prototype, "query");
+      querySpy.mockResolvedValueOnce(
+        mockLoadQueryResult() as ApolloQueryResult<any>
+      );
+      await sut.load(countryId);
+      expect(querySpy).toHaveBeenCalledWith({ query });
+    });
   });
 });
