de822db889e90f4a53f6dfe9ce32e88480d1e249
João Guilherme <joao.sczip@hotmail.com>
2020-09-03 00:24:52 -0300

    test: ensure Home display error alert on global error

--- src/presentation/pages/Home/home.spec.tsx
+++ src/presentation/pages/Home/home.spec.tsx
@@ -2,18 +2,31 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import Home from ".";
 import { LoadCountriesSpy } from "@/presentation/test";
+import { globalReducer } from "@/presentation/redux/reducers";
 
 type Sut = {
   loadCountriesSpy: LoadCountriesSpy;
 };
 
 const history = createMemoryHistory({ initialEntries: ["/"] });
-const sutFactory = (loadCountriesSpy = new LoadCountriesSpy()): Sut => {
+const initialState = {
+  global: {
+    error: null,
+  },
+};
+const sutFactory = (
+  loadCountriesSpy = new LoadCountriesSpy(),
+  state = initialState
+): Sut => {
   render(
     <Router history={history}>
-      <Home loadCountries={loadCountriesSpy} />
+      <Provider store={createStore(globalReducer, state)}>
+        <Home loadCountries={loadCountriesSpy} />
+      </Provider>
     </Router>
   );
   return {
@@ -98,5 +111,19 @@ describe("Home", () => {
     const detailsButton = container.children[0].querySelectorAll("button")[1];
     detailsButton.click();
     expect(history.location.pathname).toBe(`/country/${country.id}`);
+  });
+  it("should display an alert with error message on global error", async () => {
+    const loadCountriesSpy = new LoadCountriesSpy();
+    const error = new Error("Ops");
+    sutFactory(loadCountriesSpy, {
+      global: {
+        error,
+      },
+    });
+    await screen.findByTestId("countries-container");
+    expect(screen.queryByTestId("error-alert")).toBeInTheDocument();
+    expect(screen.queryByTestId("error-message")).toHaveTextContent(
+      error.message
+    );
   });
 });
