a6991647148cce971209bd037c00169ddea4dbbd
João Guilherme <joao.sczip@hotmail.com>
2020-09-04 14:48:21 -0300

    feat: ensure update country form shows error messages

--- src/presentation/pages/Country/components/UpdateCountry/index.tsx
+++ src/presentation/pages/Country/components/UpdateCountry/index.tsx
@@ -46,19 +46,39 @@ const UpdateCountry: React.FC<Props> = ({ openModal, setOpenModal }) => {
   const [population, setPopulation] = useState<number>();
   const [area, setArea] = useState<number>();
   const [topLevelDomains, setTopLevelDomains] = useState<string>();
+  const [formError, setFormError] = useState<boolean>(false);
   const countries = useSelector(selectCountries);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (country) {
-      const { name, capital, population, area, topLevelDomains } = country;
-      setName(name);
-      setCapital(capital);
-      setPopulation(population);
-      setArea(area);
-      setTopLevelDomains(topLevelDomains);
-    }
+    if (!country) return;
+
+    const { name, capital, population, area, topLevelDomains } = country;
+    setName(name);
+    setCapital(capital);
+    setPopulation(population);
+    setArea(area);
+    setTopLevelDomains(topLevelDomains);
   }, [country]);
+
+  const isValid = (value: any) => Boolean(value);
+
+  useEffect(() => {
+    if (!country) return;
+
+    const isInvalid = [name, capital, population, area, topLevelDomains].filter(
+      (field) => !isValid(field)
+    ).length
+      ? true
+      : false;
+
+    if (isInvalid) {
+      setFormError(true);
+      return;
+    }
+
+    setFormError(false);
+  }, [country, name, capital, population, area, topLevelDomains]);
 
   const handleSubmit = useCallback(
     (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -92,6 +112,10 @@ const UpdateCountry: React.FC<Props> = ({ openModal, setOpenModal }) => {
     ]
   );
 
+  const getValidationMessage = (value: any): string => {
+    return !isValid(value) && "Campo obrigatório.";
+  };
+
   return (
     <div data-testid="update-country">
       <Modal
@@ -120,26 +144,36 @@ const UpdateCountry: React.FC<Props> = ({ openModal, setOpenModal }) => {
                   <FormTop>
                     <div>
                       <TextField
+                        data-testid="name-group"
+                        error={!isValid(name)}
                         inputProps={{ "data-testid": "name-input" }}
                         label="Nome"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                         type="text"
                         variant="outlined"
+                        required={true}
+                        helperText={getValidationMessage(name)}
                       />
                     </div>
                     <div>
                       <TextField
+                        data-testid="capital-group"
+                        error={!isValid(capital)}
                         inputProps={{ "data-testid": "capital-input" }}
                         label="Capital"
                         value={capital}
                         onChange={(e) => setCapital(e.target.value)}
                         type="text"
                         variant="outlined"
+                        required={true}
+                        helperText={getValidationMessage(capital)}
                       />
                     </div>
                     <div>
                       <TextField
+                        data-testid="population-group"
+                        error={!isValid(population)}
                         inputProps={{ "data-testid": "population-input" }}
                         label="População"
                         value={population}
@@ -147,11 +181,15 @@ const UpdateCountry: React.FC<Props> = ({ openModal, setOpenModal }) => {
                         type="number"
                         inputMode="numeric"
                         variant="outlined"
+                        required={true}
+                        helperText={getValidationMessage(population)}
                       />
                     </div>
                   </FormTop>
                   <FormBottom>
                     <TextField
+                      data-testid="area-group"
+                      error={!isValid(area)}
                       inputProps={{ "data-testid": "area-input" }}
                       label="Área"
                       value={area}
@@ -159,19 +197,26 @@ const UpdateCountry: React.FC<Props> = ({ openModal, setOpenModal }) => {
                       type="number"
                       inputMode="numeric"
                       variant="outlined"
+                      required={true}
+                      helperText={getValidationMessage(area)}
                     />
                     <div style={{ width: "4%" }} />
                     <TextField
+                      error={!isValid(topLevelDomains)}
+                      data-testid="top-level-group"
                       inputProps={{ "data-testid": "top-level-input" }}
                       value={topLevelDomains}
                       onChange={(e) => setTopLevelDomains(e.target.value)}
                       label="Domínio de topo"
                       type="text"
                       variant="outlined"
+                      required={true}
+                      helperText={getValidationMessage(topLevelDomains)}
                     />
                   </FormBottom>
                   <div style={{ marginTop: "16px" }}>
                     <Button
+                      disabled={formError}
                       data-testid="submit"
                       type="submit"
                       color="primary"
