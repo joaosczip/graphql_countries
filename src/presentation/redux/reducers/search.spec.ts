466fcbb738d0e07a5bfc77755f29d288b185d65c
João Guilherme <joao.sczip@hotmail.com>
2020-09-07 11:03:57 -0300

    test: ensure searchReducer updates the state with search items

--- src/presentation/redux/reducers/search.spec.ts
+++ src/presentation/redux/reducers/search.spec.ts
@@ -1,6 +1,7 @@
 import faker from "faker";
-import { setSearchInput } from "@/presentation/redux/actions";
+import { setSearchInput, setSearchItems } from "@/presentation/redux/actions";
 import searchReducer from "./search";
+import { mockBasicCountries } from "@/domain/test";
 
 describe("SearchReducer", () => {
   it("should update the state with the search input", () => {
@@ -11,5 +12,14 @@ describe("SearchReducer", () => {
     const searchInput = faker.random.word();
     const state = searchReducer(initialState, setSearchInput(searchInput));
     expect(state.searchInput).toBe(searchInput);
+  });
+  it("should update the state with the search items", () => {
+    const initialState = {
+      searchInput: faker.random.word(),
+      searchItems: [],
+    };
+    const searchItems = mockBasicCountries();
+    const state = searchReducer(initialState, setSearchItems(searchItems));
+    expect(state.searchItems).toEqual(searchItems);
   });
 });
